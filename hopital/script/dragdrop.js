bf86bc19b20ac7eb7fc0779db9558920eb2571dd
Matthieu LEFÈVRE <matthieulefevre@MacBook-Air-de-Matthieu.local>
2023-11-10 03:38:18 +0100

    ajout style pour la zone de drop et alerte si pas bon fichiers

--- hopital/script/dragdrop.js
+++ hopital/script/dragdrop.js
@@ -1,11 +1,29 @@
 const depot = document.getElementById("depot");
 
 depot.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    depot.classList.add("drop");
+	e.preventDefault();
+	depot.classList.add("drop");
 });
 
 depot.addEventListener('dragleave', (e) => {
-    e.preventDefault();
-    depot.classList.remove("drop");
+	e.preventDefault();
+	depot.classList.remove("drop");
 });
+
+function drop(event) {
+	event.preventDefault();	
+	depot.classList.remove("drop");
+	console.log(event.dataTransfer.files);
+	switch (event.dataTransfer.files.length) {
+		case 0:
+			alert("Ce qui a été déposé n'est pas un fichier");
+			return;
+			break;
+		case 1:
+			break;
+		default:
+			alert("Plus de 1 fichier a été déposé");
+			return;
+			break;
+	}
+}
