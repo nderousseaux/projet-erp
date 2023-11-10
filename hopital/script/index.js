4250d4256fa664a0bde26c47469b2fa1e3a61eaa
Matthieu LEFEVRE <matthieu.lefevre@etu.unistra.fr>
2023-11-10 16:28:12 +0100

    clone de la division lors de l'appui de l'icone

--- hopital/script/index.js
+++ hopital/script/index.js
@@ -3,5 +3,10 @@ afficherRdv("confirmes");
 afficherRdv("passes");
 
 show_files = function(e) {
-	alert("okay");
+	let icon = event.target;
+	let grille = icon.parentNode.parentNode.parentNode;
+	console.log(grille);
+	var new_grille = grille.cloneNode(true);
+	new_grille.innerHTML = "test";
+	grille.parentNode.insertBefore(new_grille, grille.nextSibling);
 }
