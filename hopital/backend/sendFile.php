e7d79f5efae8e4d08612371bb0a82e63ccdd53a8
filipozzi <filipozzi@etu.unistra.fr>
2023-11-16 11:11:36 +0100

    sendFile check args

--- hopital/backend/sendFile.php
+++ hopital/backend/sendFile.php
@@ -3,4 +3,9 @@ include_once "AccesBdd.php";
 
 $bdd = new AccesBdd();
 
-$bdd->sendFile($_POST["id"], $_POST["filename"], $_POST["content"]);
+if (is_numeric($_POST["id"])) {
+	echo json_encode($bdd->sendFile($_POST["id"]), $_POST["filename"], $_POST["content"]);
+}
+else {
+	echo "Erreur: ID n'est pas un nombre";
+}
