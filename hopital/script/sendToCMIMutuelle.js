27d5b7b260bdf6a9b5a2ba0b552c60fdc045c421
filipozzi <filipozzi@etu.unistra.fr>
2023-11-16 11:43:31 +0100

    script_interconnexction

---
+++ hopital/script/sendToCMIMutuelle.js
@@ -0,0 +1,26 @@
+function sendToCMIMutuelle(cmi, content) {
+	return new Promise((resolve, reject) => {
+		// Champ à envoyer au back, pour indiquer la colonne à récupérer
+		let champPost = new FormData();
+		champPost.append("cmi", cmi);
+        champPost.append("content", content);
+
+		// Récupère les dates des mesures et les données de la colonne demandée
+		fetch("../backend/sendToCMIMutuelle.php", {
+			method: "POST",
+			body: champPost
+		})
+		.then(reponse => {
+			reponse.json()
+				.then(donnees => {
+					resolve(donnees);
+				})
+				.catch(err => {
+					reject(err);
+				})
+		})
+		.catch(err => {
+			reject(err);
+		})
+	})
+}
