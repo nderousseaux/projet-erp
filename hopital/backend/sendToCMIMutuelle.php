aaab35ca4c4524cd5d749a97d1859291c55fff49
Justin <justin.filipozzi@etu.unistra.fr>
2023-12-13 10:19:07 +0100

    debut de qqch

--- hopital/backend/sendToCMIMutuelle.php
+++ hopital/backend/sendToCMIMutuelle.php
@@ -5,6 +5,40 @@ $bdd = new AccesBdd();
 
 if (isset($_POST["idLigne"])){
 
+	$id = $_POST["idLigne"];
+	$tarif = $bdd.getTarif($id);
+	if (!$bdd->getPayeDMI($id)) {
+		
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL,
+			"http://localhost/api/data?id_grauland=value1&date=value2&heure=value3");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+
+		$output = curl_exec($ch);
+
+		curl_close($ch);
+		if ($output) {
+			$bdd.setPayeDMI($id);
+		}
+	}
+	if (!$bdd.getPayeMutuelle($id)) {
+		
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL,
+			"http://localhost/api/data?id_grauland=value1&date=value2&heure=value3");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+
+		$output = curl_exec($ch);
+
+		curl_close($ch);
+		if ($output) {
+			$bdd.setPayeMutuelle($id);
+		}
+	}
 }
 else {
 	echo "Erreur: idLigne non spécifié";
