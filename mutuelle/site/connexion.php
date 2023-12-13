6653729b1b9f2a29babbb075ce1aa1d42e8dd7a1
SZABLEWSKI THOMAS <thomas.szablewski@etu.unistra.fr>
2023-12-13 09:04:25 +0000

    GET

--- mutuelle/site/connexion.php
+++ mutuelle/site/connexion.php
@@ -12,7 +12,7 @@ function erplogin($id,$mdp,$file)
 
 #var_dump($argv);
 if(erplogin($argv[1],$argv[2],$argv[3])){
-	echo "<script>location.href='../mutuelle.php';</script>";
+	echo "<script>location.href='../mutuelle.php?nuig=".$id."';</script>";
 }else{
 	echo "t'as ecris de la merde recommence";
 };
