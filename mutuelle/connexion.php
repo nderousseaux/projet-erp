997205b153a240d65d7dc65e159da2a2cc08483e
SZABLEWSKI THOMAS <thomas.szablewski@etu.unistra.fr>
2023-12-13 08:57:50 +0000

    ca log

---
+++ mutuelle/connexion.php
@@ -0,0 +1,19 @@
+<?php
+
+function erplogin($id,$mdp,$file)
+{
+  $output = shell_exec('grep -oe "^.* '.$id.' '.$mdp.'$" '.$file.' | wc -l');
+  if ($output == "1") {
+	  return true;
+  } else {
+	  return false;
+  }
+}
+
+#var_dump($argv);
+if(erplogin($argv[1],$argv[2],$argv[3])){
+	echo "<script>location.href='../mutuelle.php';</script>";
+}else{
+	echo "t'as ecris de la merde recommence";
+};
+?>
