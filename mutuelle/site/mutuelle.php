f3537807e3c4c563370705b2ad8e8123b518edac
Iannis VALENTIN <iannis.valentin@etu.unistra.fr>
2023-12-14 18:55:27 +0100

    Avancé, mais chui une merde sur git

--- mutuelle/site/mutuelle.php
+++ mutuelle/site/mutuelle.php
@@ -2,12 +2,12 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Titre de la page</title>
+  <title>La Mutuelle</title>
   <link rel="stylesheet" href="style/style-mutuelle.css">
   <script src="script.js"></script>
 </head>
 <body>
-  <h1>Bienvenue - la mutuelle</h1>
+  <h1>Bienvenue - La Mutuelle</h1>
 <?php
 $DB='../data/mutuelle.txt';
 
@@ -16,9 +16,25 @@ function grep($id,$dbfile)
   #return shell_exec('grep -e "^[0-9\/]* '.$id.' " '.$file);
   $handle = fopen($dbfile, "r");
   if ($handle) {
+      echo '<div class="table">';
       while (($line = fgets($handle)) !== false) {
-          echo '<div>'.$line.'</div>';
+          // Séparer la ligne de la bdd en fonction des espaces
+          $elements = explode(' ', $line);
+
+          // On affiche que les éléments qui correspondent à l'utilisateur connecté
+          if($elements[1] == $id){    
+            // On sait qui on est, du coup on vire l'id
+            unset($elements[1]);
+
+            // Création des balises
+            echo '<div class="table-row">';
+            foreach ($elements as $element) {
+              echo '<div class="case">'.$element.'</div>';
+              }
+            echo '</div>';
+          }
       }
+      echo '</div>';
   
       fclose($handle);
   }
