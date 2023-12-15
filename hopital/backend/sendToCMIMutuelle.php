bf1e1ae89b751baa5d7b74a25b26892595248249
filipozzi <justin.filipozzi@etu.unistra.fr>
2023-12-15 19:03:30 +0100

    numero de port dans l'url

--- hopital/backend/sendToCMIMutuelle.php
+++ hopital/backend/sendToCMIMutuelle.php
@@ -6,18 +6,20 @@ $bdd = new AccesBdd();
 if (isset($_POST["idLigne"])){
 
 	$id = $_POST["idLigne"];
+	$num_port = 80;
 	$tarif = $bdd.getTarif($id);
-	$url = "http://localhost/api/data?id_grauland=";
+	$url = "http://localhost:";
+	$url = $url . (string)$num_port;
+	$url = $url . "/api/data?id_grauland=";
+	
 	if (!$bdd->getPayeDMI($id)) {
 		
 		$ch = curl_init();
 		$url = $url . (string)$id;
 		$url = $url . "&tarif=";
 		$url = $url . (string)($tarif / 2);
-		curl_setopt($ch, CURLOPT_URL,
-			"http://localhost/api/data?id_grauland=value1&date=value2&heure=value3");
+		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
 
 		$output = curl_exec($ch);
 
@@ -33,10 +35,8 @@ if (isset($_POST["idLigne"])){
 		$url = $url . (string)$id;
 		$url = $url . "&tarif=";
 		$url = $url . (string)($tarif / 2);
-		curl_setopt($ch, CURLOPT_URL,
-			"http://localhost/api/data?id_grauland=value1&date=value2&heure=value3");
+		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
 
 		$output = curl_exec($ch);
 
