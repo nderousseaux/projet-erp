ee533e092eeedd2c7b192343137e9cec8cc56e2e
Loïc DIEBOLT <loic@dblt.fr>
2023-12-18 13:15:10 +0100

    Correction requete confirm rdv

--- hopital/backend/confirmAppointment.php
+++ hopital/backend/confirmAppointment.php
@@ -5,7 +5,7 @@ $bdd = new AccesBdd();
 
 // Vérifie le type de rendez-vous à récupérer dans la bdd
 if (is_numeric($_POST["id"])) {
-	echo json_encode($bdd->confirmAppointment($_POST["id"]));
+	$retour = $bdd->confirmAppointment($_POST["id"]);
 
 	// Récupère les informations
 	$nomFichierBakend = "api/confirm_rdv.php";
@@ -22,6 +22,8 @@ if (is_numeric($_POST["id"])) {
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$output = curl_exec($ch);
 	curl_close($ch);
+
+	echo json_encode($retour);
 }
 else {
 	echo "Erreur: ID n'est pas un nombre";
