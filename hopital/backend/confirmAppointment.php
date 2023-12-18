494975002f0a3a71b5b53a9cdc4df8bad537acf8
Loïc DIEBOLT <loic@dblt.fr>
2023-12-18 13:04:57 +0100

    Ajout requete confirm rdv vers DMI

--- hopital/backend/confirmAppointment.php
+++ hopital/backend/confirmAppointment.php
@@ -6,6 +6,22 @@ $bdd = new AccesBdd();
 // Vérifie le type de rendez-vous à récupérer dans la bdd
 if (is_numeric($_POST["id"])) {
 	echo json_encode($bdd->confirmAppointment($_POST["id"]));
+
+	// Récupère les informations
+	$nomFichierBakend = "api/confirm_rdv.php";
+	$id = $_POST["id"];
+
+	// Forge l'url
+	$url = "http://dmi/";
+	$url = $url . $nomFichierBakend;
+	$url = $url . "?id=" . urlencode($id);
+
+	// Envoie la requête
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$output = curl_exec($ch);
+	curl_close($ch);
 }
 else {
 	echo "Erreur: ID n'est pas un nombre";
